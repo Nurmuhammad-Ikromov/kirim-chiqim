84dcdc48c0aca3e0756d37107e523ea0d858c8ef
unknown <nurmuhammadikromov58@gmail.com>
2024-12-25 14:03:43 +0500

    Fix errors

--- src/models/ExpenseModel.js
+++ src/models/ExpenseModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const ExpenseSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true,
+    required:  true,
   },
   amount: {
     type: Number,
